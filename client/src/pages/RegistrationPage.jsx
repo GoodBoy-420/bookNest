704c40028d5952f476e5d3689e3406983087c2bc
Riad <riadislam9117@gmail.com>
2026-02-04 15:54:48 +0600

    Registration and Login done ✔️

--- client/src/pages/RegistrationPage.jsx
+++ client/src/pages/RegistrationPage.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import RegistrationImage from "../assets/registration.jpg";
 
 import RegistrtationForm from "../components/auth/RegistrtationForm";
@@ -7,8 +9,14 @@ const RegistrationPage = () => {
       <div className="flex h-175 w-full">
         <div className="w-full flex flex-col items-center justify-center">
           <RegistrtationForm />
-        </div>
 
+          <p className="text-gray-500/90 text-sm mt-4">
+            Already Have an account?
+            <Link className="text-indigo-400 hover:underline" to="/signin">
+              Sign in
+            </Link>
+          </p>
+        </div>
         <div className="w-full hidden md:inline-block">
           <img
             className="h-full ml-10"
